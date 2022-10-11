2941f16a6b96a0345d6d7d743fd6b5fae13472e3
Nick Phelps <nickphelps34@gmail.com>
2022-10-11 17:14:05 -0400

    commit tree 62c0c39f4465fae6539a94902432340d21d2d17c

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someDistance) {
+    if (someDistance === 43)
+    return 1
+    else if (someDistance === 50 , 34 )
+    return 8
+};
+
+function distanceFromHqInFeet (someDistance) {
+    if (someDistance === 43)
+        return 264
+    else (someDistance === 50 , 34)
+        return 2112
+};
+function distanceTravelledInFeet(start, destination) {
+    
+    let feetTravelled =(destination - start) * 264;
+    return Math.abs(feetTravelled);
+}
+
+
+function calculatesFarePrice(start, destination) {
+    let totalFeetTravelled = distanceTravelledInFeet(start, destination);
+        if (totalFeetTravelled <= 400) {
+        return 0;
+        }
+        else if (totalFeetTravelled > 400 && totalFeetTravelled <= 2000){
+            return .02 * (totalFeetTravelled - 400);
+        }
+        else if (totalFeetTravelled > 2000 && totalFeetTravelled < 2500){
+        return 25;
+        }
+        else {
+            return "cannot travel that far"
+        }
+}
